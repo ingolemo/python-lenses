0b7025a83b9db17e541096261623a10a2de35e64
Adrian Room <ingolemo@gmail.com>
2016-01-27 00:20:24 +0000

    factored out userlens tests

--- tests/test_lens.py
+++ tests/test_lens.py
@@ -103,74 +103,13 @@ def test_type_unsupported_no_setter():
         lens(object()).attr.set(None)
 
 
-# Tests for using lens combinators through a BoundLens
-def test_boundlens_get():
-    assert lens(10).get() == 10
-    assert lens([1, 2, 3])[1].get() == 2
-
-
-def test_boundlens_get_all():
-    my_lens = lenses.both().compose(lenses.getitem(1))
-    assert lens([[1, 2], [3, 4]], my_lens).get_all() == (2, 4)
-
-
-def test_boundlens_set():
-    assert lens(10).set(5) == 5
-    assert lens([1, 2, 3])[1].set(5) == [1, 5, 3]
-
-
-def test_boundlens_modify():
-    assert lens(10).modify(lambda a: a + 1) == 11
-    assert lens([1, 2, 3])[0].modify(lambda a: a + 5) == [6, 2, 3]
-
-
-def test_boundlens_call_method():
-    assert lens('hello').call_method('upper') == 'HELLO'
-
-
-def test_boundlens_call_method_args():
-    assert lens('h').call_method('center', 5) == '  h  '
-
-
-def test_boundlens_call_method_kwargs():
-    assert lens('h').call_method('encode', encoding='utf-8') == b'h'
-
-
-def test_boundlens_add_lens():
-    assert lens([1, 2]).add_lens(lenses.trivial()) + [3] == [1, 2, 3]
-    assert lens([1, 2]).add_lens(lenses.getitem(1)).set(3) == [1, 3]
-
-
-def test_boundlens_add():
-    assert lens(2) + 1 == 3
-    assert lens([[1, 2], 3])[0] + [4] == [[1, 2, 4], 3]
-
-
-def test_boundlens_subtract():
-    assert lens(2) - 1 == 1
-
-
-def test_boundlens_multiply():
-    assert lens(2) * 2 == 4
-    assert lens([[1, 2], [3]])[1] * 3 == [[1, 2], [3, 3, 3]]
-
-
-def test_boundlens_divide():
-    assert lens(10) / 2 == 5
-
-
-def test_boundlens_informative_repr():
-    obj = object()
-    assert repr(obj) in repr(lens(obj))
-
-
+# Tests for lenses and lens constructor function that are built into the
+# library.
 def test_lens_and():
     my_lens = lenses.both() & lenses.getitem(1)
     assert my_lens.set([(0, 1), (2, 3)], 4) == [(0, 4), (2, 4)]
 
 
-# Tests for lenses and lens constructor function that are built into the
-# library.
 def test_lens_getattr_l():
     Tup = collections.namedtuple('Tup', 'attr')
     obj = Tup(1)
---
+++ tests/test_userlens.py
@@ -0,0 +1,67 @@
+import collections
+
+import pytest
+
+import lenses
+from lenses import lens
+
+
+# Tests for using lens combinators through a UserLens
+def test_userlens_get():
+    assert lens(10).get() == 10
+    assert lens([1, 2, 3])[1].get() == 2
+
+
+def test_userlens_get_all():
+    my_lens = lenses.both().compose(lenses.getitem(1))
+    assert lens([[1, 2], [3, 4]], my_lens).get_all() == (2, 4)
+
+
+def test_userlens_set():
+    assert lens(10).set(5) == 5
+    assert lens([1, 2, 3])[1].set(5) == [1, 5, 3]
+
+
+def test_userlens_modify():
+    assert lens(10).modify(lambda a: a + 1) == 11
+    assert lens([1, 2, 3])[0].modify(lambda a: a + 5) == [6, 2, 3]
+
+
+def test_userlens_call_method():
+    assert lens('hello').call_method('upper') == 'HELLO'
+
+
+def test_userlens_call_method_args():
+    assert lens('h').call_method('center', 5) == '  h  '
+
+
+def test_userlens_call_method_kwargs():
+    assert lens('h').call_method('encode', encoding='utf-8') == b'h'
+
+
+def test_userlens_add_lens():
+    assert lens([1, 2]).add_lens(lenses.trivial()) + [3] == [1, 2, 3]
+    assert lens([1, 2]).add_lens(lenses.getitem(1)).set(3) == [1, 3]
+
+
+def test_userlens_add():
+    assert lens(2) + 1 == 3
+    assert lens([[1, 2], 3])[0] + [4] == [[1, 2, 4], 3]
+
+
+def test_userlens_subtract():
+    assert lens(2) - 1 == 1
+
+
+def test_userlens_multiply():
+    assert lens(2) * 2 == 4
+    assert lens([[1, 2], [3]])[1] * 3 == [[1, 2], [3, 3, 3]]
+
+
+def test_userlens_divide():
+    assert lens(10) / 2 == 5
+
+
+def test_userlens_informative_repr():
+    obj = object()
+    assert repr(obj) in repr(lens(obj))
