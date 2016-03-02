fffd70a3c5895aa3e2741f25a003ed5ffa986dd4
Adrian Room <ingolemo@gmail.com>
2016-03-02 13:43:58 +0000

    cleaned up robots.py example

--- examples/robots.py
+++ examples/robots.py
@@ -2,11 +2,13 @@
 
 import contextlib
 import curses
-from random import randrange
+import curses.textpad
+from random import randint
 
 from lenses import lens
 
-SIZE = 20
+MIN = 1
+MAX = 20
 ROBOTS = 6
 
 
@@ -30,11 +32,11 @@ def curses_context():
 
 
 def restrain(n):
-    return max(0, min(n, SIZE - 1))
+    return max(MIN, min(n, MAX))
 
 
 def cmp(a, b):
-    return -1 if a < b else 1 if a > b else 0
+    return (a > b) - (a < b)
 
 
 class GameState:
@@ -47,25 +49,27 @@ class GameState:
         self.message = None
 
         for _ in range(ROBOTS):
-            robot_coord = randrange(SIZE), randrange(SIZE)
+            robot_coord = randint(MIN, MAX), randint(MIN, MAX)
             self.robots.add(robot_coord)
 
     def handle_input(self, input):
         dirs = {
             'h': (-1, 0), 'j': (0, 1), 'k': (0, -1), 'l': (1, 0),
             'y': (-1, -1), 'u': (1, -1), 'n': (1, 1), 'b': (-1, 1),
+            '.': (0, 0),
         }
 
-        if input == 'q':
-            return self.end_game(), False
-        elif input == 't':
-            return lens(self).player.both_().modify(
-                lambda a: randrange(SIZE)), True
-        elif input in dirs:
+        if input in dirs:
             dx, dy = dirs[input]
             self = lens(self).player[0] + dx
             self = lens(self).player[1] + dy
             self = lens(self).player.both_().modify(restrain)
+            return self, True
+        elif input == 'q':
+            return self.end_game(), False
+        elif input == 't':
+            self = lens(self).player.both_().modify(
+                lambda a: randint(MIN, MAX))
             return self, True
         else:
             return self, False
@@ -86,10 +90,12 @@ class GameState:
                 new_robots.add(new_coord)
         self = lens(self).robots.set(new_robots)
         self = lens(self).crashes.set(crashes)
+        return self
 
-        if self.player in new_robots | crashes:
+    def check_game_end(self):
+        if self.player in self.robots | self.crashes:
             return self.end_game('You Died!')
-        elif not new_robots:
+        elif not self.robots:
             return self.end_game('You Win!')
         else:
             return self
@@ -99,6 +105,7 @@ class GameState:
         return lens(self).running.set(False)
 
     def draw(self, screen):
+        curses.textpad.rectangle(screen, MIN-1, MIN-1, MAX+1, MAX+1)
         for robot in self.robots:
             screen.addstr(robot[1], robot[0], 'O')
         for crash in self.crashes:
@@ -116,13 +123,14 @@ def main():
             state, should_advance = state.handle_input(input)
             if should_advance:
                 state = state.advance_robots()
+                state = state.check_game_end()
 
             screen.clear()
             state.draw(screen)
             screen.refresh()
 
         if state.message:
-            screen.addstr(9, 4, state.message)
+            screen.addstr(0, 2, state.message)
             screen.getch()
 
 
