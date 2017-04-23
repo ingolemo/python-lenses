f8a71be2a8d8539ca9e9756548098065af9bb1c7
Adrian Room <ingolemo@gmail.com>
2017-04-23 11:38:31 +0100

    move doctests to after the pytest tests
    the pytest tests give more useful output on failure, so we want them to
    run (and potentially fail) first.

--- runtests.sh
+++ runtests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-python -m doctest readme.md
-test "$?" = '0' || exit
-
 coverage run -m pytest lenses tests --doctest-modules "$@"
 test "$?" = '0' || exit
 
 coverage report -m
+
+python -m doctest readme.md
+test "$?" = '0' || exit
