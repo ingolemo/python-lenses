024e87af074877bb11d04210a38e9954cce1ba0a
Adrian Room <ingolemo@gmail.com>
2017-05-04 17:18:14 +0100

    removed useless file test_typeclass.py

--- tests/test_typeclass.py
+++
@@ -1,7 +0,0 @@
-import lenses.typeclass as tc
-from lenses.functorisor import Functorisor
-from lenses.identity import Identity
-from lenses.maybe import Just, Nothing
-
-ident = Functorisor(lambda a: Identity(a), lambda a: Identity(4))
-
