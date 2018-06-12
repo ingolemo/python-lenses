265e82ff29b8cf703ec2942f3842347b22dbc36a
Adrian Room <ingolemo@gmail.com>
2018-06-12 20:31:06 +0100

    reformatted lenses.optics imports to be more diff-friendly

--- lenses/optics/__init__.py
+++ lenses/optics/__init__.py
@@ -1,16 +1,46 @@
 from .base import (
-    LensLike, Fold, Setter, Getter, Traversal, Lens, Review, Prism,
-    Isomorphism, Equality, ComposedLens, TrivialIso, ErrorIso
+    LensLike,
+    Fold,
+    Setter,
+    Getter,
+    Traversal,
+    Lens,
+    Review,
+    Prism,
+    Isomorphism,
+    Equality,
+    ComposedLens,
+    TrivialIso,
+    ErrorIso,
 )
 from .folds import IterableFold
-from .isomorphisms import (DecodeIso, JsonIso, ListWrapIso, NormalisingIso)
-from .prisms import (FilteringPrism, InstancePrism, JustPrism)
+from .isomorphisms import (
+    DecodeIso,
+    JsonIso,
+    ListWrapIso,
+    NormalisingIso,
+)
+from .prisms import (
+    FilteringPrism,
+    InstancePrism,
+    JustPrism,
+)
 from .setters import ForkedSetter
 from .true_lenses import (
-    ContainsLens, GetattrLens, GetitemLens, GetitemOrElseLens, ItemLens,
-    ItemByValueLens, TupleLens
+    ContainsLens,
+    GetattrLens,
+    GetitemLens,
+    GetitemOrElseLens,
+    ItemLens,
+    ItemByValueLens,
+    TupleLens,
 )
 from .traversals import (
-    BothTraversal, EachTraversal, GetZoomAttrTraversal, ItemsTraversal,
-    RecurTraversal, ZoomAttrTraversal, ZoomTraversal
+    BothTraversal,
+    EachTraversal,
+    GetZoomAttrTraversal,
+    ItemsTraversal,
+    RecurTraversal,
+    ZoomAttrTraversal,
+    ZoomTraversal,
 )
